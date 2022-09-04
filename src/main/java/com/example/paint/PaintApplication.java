62f4c5c4ad0e9f8c7bf35d1549b35878e4642ceb
engelsteve0 <engelsteve0@gmail.com>
2022-09-03 22:56:42 -0500

    Beginnings of menu, canvas...
    Signed-off-by: engelsteve0 <engelsteve0@gmail.com>

--- src/main/java/com/example/paint/PaintApplication.java
+++ src/main/java/com/example/paint/PaintApplication.java
@@ -1,6 +1,8 @@
 package com.example.paint;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -8,6 +10,9 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
@@ -24,28 +29,37 @@ public class PaintApplication extends Application {
         grid.setVgap(10);
         //grid.setPadding(new Insets(25, 25, 25, 25));
 
-
-        final Menu fileMenu = new Menu("File"); //creates the menu bar across the top. Menus are subject to change.
-        final Menu optionsMenu = new Menu("View");
-        final Menu helpMenu = new Menu("About");
+        Menu fileMenu = new Menu("File"); //creates the menu bar across the top. Menus are subject to change.
+        MenuItem saveDD = new MenuItem("Save (Ctrl + S)");
+        saveDD.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                System.out.println("Saving...");
+            }
+        });
+        fileMenu.getItems().add(saveDD);
+        Menu optionsMenu = new Menu("View");
+        Menu helpMenu = new Menu("About");
         MenuBar menuBar = new MenuBar();
         menuBar.getMenus().addAll(fileMenu, optionsMenu, helpMenu);
+
         grid.add(menuBar, 0, 0); //adds to top left of window
 
-        final Canvas canvas = new Canvas(250,250);
+        Canvas canvas = new Canvas(250,250);
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
-        gc.setFill(Color.BLUE);
+        gc.setFill(Color.WHITE);
         gc.fillRect(75,75,100,100);
         grid.add(canvas, 0, 1);
 
-        Scene scene = new Scene(grid, 300, 275); //makes a scene (pun intended)
+        Scene scene = new Scene(grid, 600, 600); //makes a scene (pun intended)
         stage.setScene(scene);
         stage.setTitle("Paint");
 
-        StackPane root = new StackPane(); //Adds grid to stackpane
-        root.getChildren().add(grid);
-        stage.setScene(new Scene(root, 300, 250));
+        //StackPane root = new StackPane(); //Adds grid to stackpane
+        //root.getChildren().add(grid);
+        //stage.setScene(new Scene(root, 300, 250));
+
+        stage.setResizable(true);
         stage.show(); //Displays stage which hosts the scene which hosts the grid
     }
 
